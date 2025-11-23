4a9c07935e414a65997be6e370de7fc43d9d90bb
Behruz Jumanov <behruzjumanov55@gmail.com>
2025-11-23 12:18:15 +0500

    your message

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
